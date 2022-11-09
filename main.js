2726a95c2c8060a94ed381cc89dc6b6e0de56c7a
Santiago Mansilla <santimasig@outlook.com.ar>
2022-11-08 21:46:28 -0300

    Simulador compras y gastos

--- main.js
+++ main.js
@@ -1,117 +1,135 @@
-// Una aplicacion para calcular el importe total y unitario en cuotas en base a una tasa de interes
+totalIngresos = 0;
+totalGastos = 0;
 
-function calculo_cuotas() {
-  let acumulado = 0;
-  let arr_cuotas = [""];
+// funciones sumadoras
 
-  do {
-    n1 = prompt("Ingrese Capital", "10000");
-  } while (n1.trim() == "");
-
-  do {
-    n2 = prompt("Ingrese la cantidad de cuotas", "12");
-  } while (n2.trim() == "");
-
-  do {
-    n3 = prompt("Ingrese tasa de interes", "7.5");
-  } while (n3.trim() == "");
-
-  //Creo Objeto con propiedades y funciones
-  const credito = {
-    importe_capital: n1,
-    numero_cuotas: n2,
-    tasa_interes: n3,
-
-    presentacion: function () {
-      return (
-        "Simularemos un prestamo de " +
-        this.importe_capital +
-        " en " +
-        this.numero_cuotas +
-        " cuotas con una tasa de interes del " +
-        this.tasa_interes +
-        "%"
-      );
-    },
-
-    capitalXcuota: function () {
-      return Number(this.importe_capital / this.numero_cuotas).toFixed(2);
-    },
-
-    interesXcuota: function () {
-      return (
-        (Number(this.importe_capital / this.numero_cuotas) *
-          this.tasa_interes) /
-        100
-      ).toFixed(2);
-    },
-
-    importeXcuota: function () {
-      return (
-        Number(this.importe_capital / this.numero_cuotas) +
-        (Number(this.importe_capital / this.numero_cuotas) *
-          this.tasa_interes) /
-          100
-      ).toFixed(2);
-    },
-  };
-
-  //Ejecuto una funcion del objeto
-  console.log(credito.presentacion());
-  //console.log("capitalXcuota: "+credito.capitalXcuota());
-  //console.log("interesXcuota: "+credito.interesXcuota());
-  //console.log("importeXcuota: "+credito.importeXcuota());
-
-  function Acumular(imp) {
-    return (acumulado += Number(imp));
+function sumarIngresos() {
+  for (let i = 0; i < misIngresos.length; i++) {
+    totalIngresos = totalIngresos + misIngresos[i].monto;
   }
-
-  let inicio = Date.now();
-
-  // Muestro los datos de cada cuota
-  for (i = 1; i < Number(credito.numero_cuotas) + 1; i++) {
-    console.log(
-      "Cuota nro " +
-        i +
-        ": Capital $" +
-        credito.capitalXcuota() +
-        " Interes $" +
-        credito.interesXcuota() +
-        " Total $" +
-        credito.importeXcuota() +
-        " Acumulado $" +
-        Acumular(credito.importeXcuota()).toFixed(2)
-    );
-
-    // Creo Array para buscar la cuota luego de hacer los calculos
-    arr_cuotas.push(
-      "Cuota nro " +
-        i +
-        ": Capital $" +
-        credito.capitalXcuota() +
-        " Interes $" +
-        credito.interesXcuota() +
-        " Total $" +
-        credito.importeXcuota()
-    ); //+" Acumulado $"+ Acumular(credito.importeXcuota()).toFixed(2));
-  }
-
-  let final = Date.now();
-  let ejecucion = final - inicio;
-  console.log("Tiempo de ejecucion del for " + ejecucion + " milisegundos.");
-
-  do {
-    busqueda_cuota = prompt("Ingrese el numero de cuota que quiere ver", "1");
-  } while (busqueda_cuota.trim() == "");
-
-  console.log(arr_cuotas[busqueda_cuota]);
+  return totalIngresos;
 }
 
+function sumarGastos() {
+  for (let i = 0; i < misGastos.length; i++) {
+    totalGastos = totalGastos + misGastos[i].monto;
+  }
+  return totalGastos;
+}
 
+// clase constructora de objetos
 
+class Dinero {
+  constructor(concepto, monto) {
+    this.concepto = concepto.toUpperCase();
+    this.monto = parseInt(monto);
+  }
+}
 
+// Arrays agrupando objetos de ingreso/gasto
+const misIngresos = [];
+const misGastos = [];
 
+// CARGA DE INGRESOS DEL USUARIO
 
+for (let i = 1; i < 11; i++) {
+  alert("Describa el ingreso n° " + i);
+  let conceptoIngreso = prompt(
+    "Concepto del ingreso. Si desea salir sin colocar más nada, escriba " +
+      "ESC"
+  );
+  conceptoIngreso = conceptoIngreso.toUpperCase();
+
+  if (conceptoIngreso != "ESC") {
+    let montoIngreso = prompt("Monto del ingreso:");
+
+    while (isNaN(montoIngreso) == true || parseInt(montoIngreso) < 0) {
+      alert("ERROR. Sólo carácteres numéricos mayores a 0 válidos.");
+      montoIngreso = prompt("Coloque nuevamente el monto del ingreso:");
+      if (isNaN(montoIngreso) == false && parseInt(montoIngreso) > 0) {
+        break;
+      }
+    }
+    misIngresos.push(new Dinero(conceptoIngreso, montoIngreso)); //cargo al array de ingresos, el que cumplió el ciclo de validación con éxito
+  } else {
+    break;
+  }
+}
+
+// CARGA DE GASTOS DEL USUARIO
+
+for (let i = 1; i < 11; i++) {
+  alert("Describa el gasto n° " + i);
+  let conceptoGasto = prompt(
+    "Concepto del gasto. Si desea salir sin colocar más nada, escriba " + "ESC"
+  );
+  conceptoGasto = conceptoGasto.toUpperCase();
+
+  if (conceptoGasto != "ESC") {
+    let montoGasto = prompt("Monto del gasto:");
+
+    while (isNaN(montoGasto) == true || parseInt(montoGasto) < 0) {
+      alert("ERROR. Sólo carácteres numéricos mayores a 0 válidos.");
+      montoGasto = prompt("Coloque nuevamente el monto del gasto:");
+      if (isNaN(montoGasto) == false && parseInt(montoGasto) > 0) {
+        break;
+      }
+    }
+    misGastos.push(new Dinero(conceptoGasto, montoGasto)); //cargo al array de gastos, el que cumplió el ciclo de validación con éxito
+  } else {
+    break;
+  }
+}
+
+// MUESTRA DE SALDOS
+
+sumarIngresos();
+sumarGastos();
+let saldo = totalIngresos - totalGastos;
+
+alert(
+  "Tienes un total de ingresos por la cantidad de: $" +
+    totalIngresos +
+    "\n" +
+    "Tus gastos de este mes, suman: $" +
+    totalGastos +
+    "\n" +
+    "Por lo tanto, tu saldo es de: $" +
+    saldo
+);
+
+// MUESTRA DE INFO DETALLADA
+
+let mensajeIngresos = "";
+for (let i = 0; i < misIngresos.length; i++) {
+  mensajeIngresos +=
+    "Concepto: " +
+    misIngresos[i].concepto +
+    " | Monto: $" +
+    misIngresos[i].monto +
+    "\n";
+}
+
+let mensajeGastos = "";
+for (let i = 0; i < misGastos.length; i++) {
+  mensajeGastos +=
+    "Concepto: " +
+    misGastos[i].concepto +
+    " | Monto: $" +
+    misGastos[i].monto +
+    "\n";
+}
+
+alert(
+  "\n" +
+    "Tus ingresos:" +
+    "\n" +
+    mensajeIngresos +
+    "\n" +
+    "\n" +
+    "Tus Gastos" +
+    "\n" +
+    mensajeGastos);
 
 /* Simulador de autos
   class Autos {
